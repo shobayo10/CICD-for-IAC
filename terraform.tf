1b2105909bb10de4f88954345f4b83d67427c28e
Shobayo Adeolu <49585237+shobayo10@users.noreply.github.com>
2024-01-04 12:44:36 +0000

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -3,7 +3,7 @@ terraform {
 
   backend "s3" {
     key    = "github-actions-cicd/terraform.tfstate" # the directory/file.tfstate
-    bucket = "ADD_YOUR_BUCKET_NAME_HERE"             # the bucket
-    region = "ADD_YOUR_REGION_NAME_HERE"             # the region
+    bucket = "cicd-4-iac"             # the bucket
+    region = "us-west-2"             # the region
   }
 }
